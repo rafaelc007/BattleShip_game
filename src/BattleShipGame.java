936047e51eff1298ce058ddb3a8ac67a78028f1b
rafaelc007 <raf07ray@gmail.com>
2020-01-08 08:06:39 -0400

    fixed some warnings.

--- src/BattleShipGame.java
+++ src/BattleShipGame.java
@@ -1,13 +1,16 @@
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BattleShipGame {
-    private ArrayList<Ship> shipList = new ArrayList<Ship>();
+    private ArrayList<Ship> shipList = new ArrayList<>();
     private int score = 0;
     private int[] gridSize = {7, 7};
     private TerminalGraphics gameGraph = new TerminalGraphics(gridSize);
-    private ArrayList<String> sortedOnes = new ArrayList<String>();
+    private ArrayList<String> sortedOnes = new ArrayList<>();
+
+    final protected void setSortedOnes(ArrayList<String> sorted) {this.sortedOnes = sorted; }
 
     final public void setGridSize(int valX, int valY) {
         if(valX > 0 && valX < 26) {
@@ -42,7 +45,7 @@ public class BattleShipGame {
         return false;
     }
 
-    protected String makeAPos(int x, int y) {
+    final protected String makeAPos(int x, int y) {
         // make a grid pos string out of a euclidian numeric position
         if(x >= 0 && x < this.gridSize[0] && y >=0 && y < this.gridSize[1]) {
             return (char) (x + 65) + String.valueOf(y);
@@ -50,7 +53,7 @@ public class BattleShipGame {
         return null;
     }
 
-    protected int[] makeAPos(String position) {
+    final protected int[] makeAPos(String position) {
         // make a euclidian numeric position out of a grid pos string
         int intPosX = (int) position.charAt(0) - 65;
         int intPosY = (int) position.charAt(1) - 48;
@@ -62,7 +65,7 @@ public class BattleShipGame {
         return null;
     }
 
-    protected String[] sortPositions(int shipSize) {
+    final protected String[] sortPositions(int shipSize) {
         int genSeedX = (int) Math.floor(Math.random() * (this.gridSize[0]-shipSize));
         int genSeedY = (int) Math.floor(Math.random() * (this.gridSize[1]-shipSize));
         byte shipOrient = (byte) Math.round(Math.random());     // 0: vertical, 1: horizontal
@@ -74,17 +77,17 @@ public class BattleShipGame {
         return genPositions;
     }
 
-    private boolean compareSorted(String[] sorted) {
+    final protected boolean compareSorted(String[] sorted) {
         int i = 0;
         for(String val : this.sortedOnes) {
-            if(sorted[i] == val) {
+            if(sorted[i].equals(val)) {
                 return true;
             }
         }
         return false;
     }
 
-    protected void initializeGame(int numOfShips, int shipSize) {
+    final protected void initializeGame(int numOfShips, int shipSize) {
         for(int i=0; i<numOfShips; ++i) {
             Ship gameShip = new Ship(shipSize);
 
@@ -104,7 +107,7 @@ public class BattleShipGame {
         }
     }
 
-    protected String askPlayer() {
+    final protected String askPlayer() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a position: ");
         String playerGuess = scan.nextLine().toUpperCase();
@@ -143,20 +146,20 @@ public class BattleShipGame {
                             hitNow = true;
                             if (result == 1) {
                                 System.out.println("hit on ship " + shipNum);
-                                gameGraph.setBoardPos(this.makeAPos(playerGuess), result);
+                                gameGraph.setBoardPos(Objects.requireNonNull(this.makeAPos(playerGuess)), result);
                             } else if (result == 2) {
                                 System.out.print("already hit on ship " + shipNum);
                             }
                             else if (result == 3) {
                                 System.out.println("hit, ship " + shipNum + " sunk");
-                                gameGraph.setBoardPos(this.makeAPos(playerGuess), 1);
+                                gameGraph.setBoardPos(Objects.requireNonNull(this.makeAPos(playerGuess)), 1);
                             }
                         }
                         shipNum++;
                     }
                     if (!hitNow) {
                         System.out.println("miss");
-                        gameGraph.setBoardPos(this.makeAPos(playerGuess), 3);
+                        gameGraph.setBoardPos(Objects.requireNonNull(this.makeAPos(playerGuess)), 3);
                     }
                 }
                 this.score++;
