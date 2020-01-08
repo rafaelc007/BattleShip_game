50d31444b26895ac92d7a974f84f1862ab890a7a
rafaelc007 <raf07ray@gmail.com>
2020-01-08 08:10:14 -0400

    created a test case for compareSorted.

--- src/BattleShipTestDrive.java
+++ src/BattleShipTestDrive.java
@@ -1,15 +1,19 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class BattleShipTestDrive extends BattleShipGame{
     BattleShipGame game = new BattleShipGame();
 
     private void TestMakeAPos() {
         this.game.setGridSize(10, 10);  //max is J9
         System.out.println("Grid size: " + this.game.getGridSize());
-        assert this.game.makeAPos(7, 7).equals("H7");
-        assert this.game.makeAPos(0, 0).equals("A0");
+        assert Objects.requireNonNull(this.game.makeAPos(7, 7)).equals("H7");
+        assert Objects.requireNonNull(this.game.makeAPos(0, 0)).equals("A0");
         assert this.game.makeAPos(26, 0) == null;
         assert this.game.makeAPos(0, 11) == null;
 
         int[] pos1 = this.game.makeAPos("B3");
+        assert pos1 != null;
         assert pos1[0] == 1;
         assert pos1[1] == 3;
     }
@@ -33,11 +37,22 @@ public class BattleShipTestDrive extends BattleShipGame{
         this.game.playGame(3);
     }
 
+    private void TestCompareSorted() {
+        String[] testSorted = {"A1", "A2", "A3"};
+        ArrayList<String> sortedList = new ArrayList<>();
+        sortedList.add("A2");
+        sortedList.add("B0");
+        this.game.setSortedOnes(sortedList);
+        assert this.game.compareSorted(testSorted);
+        System.out.println("passed!");
+    }
+
     public static void main(String[] args) {
         BattleShipTestDrive driver = new BattleShipTestDrive();
-        driver.TestMakeAPos();
+        //driver.TestMakeAPos();
         //driver.TestInitializeGame();
         //driver.TestAskPlayer();
         //driver.TestPlayGame();
+        driver.TestCompareSorted();
     }
 }
